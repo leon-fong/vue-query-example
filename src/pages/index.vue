c42cc09a50d0dc8053460a769a22e9f36a4bf66f
fanglm <ooohmydawn@hotmail.com>
2023-03-09 17:51:45 +0800

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+defineOptions({
+  name: 'IndexPage',
+})
+
+const name = $ref('')
+
+const router = useRouter()
+const go = () => {
+  if (name)
+    router.push(`/hi/${encodeURIComponent(name)}`)
+}
+</script>
+
+<template>
+  <div>
+    hello
+  </div>
+</template>
